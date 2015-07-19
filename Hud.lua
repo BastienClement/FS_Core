31e7937962c3337bb8e74edca77ead3b910acf22
galedric <g@ledric.me>
2015-07-19 14:20:08 +0200

    Draw HUD lines

--- Hud.lua
+++ Hud.lua
@@ -23,12 +23,6 @@ function Hud:OnInitialize()
 end
 
 function Hud:OnEnable()
-	self:DrawUnit("player")
-	
-	C_Timer.After(5, function()
-		local x, y = UnitPosition("player")
-		self:DrawPoint(x, y, { 0, 1, 0 })
-	end)
 end
 
 function Hud:OnDisable()
@@ -149,25 +143,24 @@ function HudObject:SetTex(path)
 	self.tex:Show()
 end
 
-function HudObject:SetTexColor(r, g, b)
-	self.tex:SetVertexColor(r, g, b)
+function HudObject:SetTexColor(...)
+	self.tex:SetVertexColor(...)
 end
 
 function HudObject:Update()
+	if self.OnUpdate then
+		self:OnUpdate()
+	end
+	self:Draw()
+end
+
+function HudObject:Draw()
 	local x, y = self:Position()
 	if not x then
 		self:Remove()
-	else
-		if obj.OnUpdate then
-			obj:OnUpdate()
-		end
-		self:Draw(x, y)
+		return
 	end
-end
-
-function HudObject:Draw(x, y)
-	local rx, ry = Hud:Project(x, y)
-	obj.frame:SetPoint("CENTER", hud, "CENTER", rx, ry)
+	self.frame:SetPoint("CENTER", hud, "CENTER", Hud:Project(x, y))
 end
 
 function Hud:CreateObject(proto)
@@ -220,7 +213,11 @@ do
 	local gray = { 0.5, 0.5, 0.5 }
 	
 	function Hud:DrawPoint(x, y, color)
-		local p = self:CreateObject({ x = x or 0, y = y or 0, color = color or gray })
+		local p = self:CreateObject({
+			x = x or 0,
+			y = y or 0,
+			color = color or gray
+		})
 		
 		function p:Init()
 			self:SetSize(16, 16)
@@ -233,6 +230,85 @@ do
 		end
 		
 		return self:AddObject(p)
+	end
+end
+
+-- Line
+do
+	local gray = { 0.5, 0.5, 0.5 }
+	
+	local TAXIROUTE_LINEFACTOR = 32 / 30
+	local TAXIROUTE_LINEFACTOR_2 = TAXIROUTE_LINEFACTOR / 2
+
+	function Hud:DrawLine(sx, sy, ex, ey, width, color)
+		local l = self:CreateObject({
+			sx = sx or 0,
+			sy = sy or 0,
+			ex = ex or 0,
+			ey = ey or 0,
+			width = width or 32,
+			color = color or gray
+		})
+		
+		function l:Init()
+			--self:SetTex("Interface\\TaxiFrame\\UI-Taxi-Line")
+			self:SetTex("Interface\\AddOns\\FS_Core\\media\\line")
+			self:SetTexColor(unpack(self.color))
+		end
+		
+		function l:Draw()
+			local sx, sy = Hud:Project(self.sx, self.sy)
+			local ex, ey = Hud:Project(self.ex, self.ey)
+			
+			-- Determine dimensions and center point of line
+			local dx, dy = ex - sx, ey - sy
+			local cx, cy = (sx + ex) / 2, (sy + ey) / 2
+			local w = self.width
+			
+			-- Normalize direction if necessary
+			if dx < 0 then
+				dx, dy = -dx, -dy;
+			end
+			
+			-- Calculate actual length of line
+			local l = (dx * dx + dy * dy) ^ 0.5
+			
+			-- Quick escape if it's zero length
+			if l == 0 then
+				self.frame:ClearAllPoints()
+				self.frame:SetPoint("BOTTOMLEFT", hud, "CENTER", cx, cy)
+				self.frame:SetPoint("TOPRIGHT",   hud, "CENTER", cx, cy)
+				self.tex:SetTexCoord(0,0,0,0,0,0,0,0)
+				return
+			end
+			
+			-- Sin and Cosine of rotation, and combination (for later)
+			local s, c = -dy / l, dx / l
+			local sc = s * c
+			
+			-- Calculate bounding box size and texture coordinates
+			local Bwid, Bhgt, BLx, BLy, TLx, TLy, TRx, TRy, BRx, BRy
+			if dy >= 0 then
+				Bwid = ((l * c) - (w * s)) * TAXIROUTE_LINEFACTOR_2
+				Bhgt = ((w * c) - (l * s)) * TAXIROUTE_LINEFACTOR_2
+				BLx, BLy, BRy = (w / l) * sc, s * s, (l / w) * sc
+				BRx, TLx, TLy, TRx = 1 - BLy, BLy, 1 - BRy, 1 - BLx
+				TRy = BRx;
+			else
+				Bwid = ((l * c) + (w * s)) * TAXIROUTE_LINEFACTOR_2
+				Bhgt = ((w * c) + (l * s)) * TAXIROUTE_LINEFACTOR_2
+				BLx, BLy, BRx = s * s, -(l / w) * sc, 1 + (w / l) * sc
+				BRy, TLx, TLy, TRy = BLx, 1 - BRx, 1 - BLx, 1 - BLy
+				TRx = TLy
+			end
+			
+			self.frame:ClearAllPoints()
+			self.frame:SetPoint("BOTTOMLEFT", hud, "CENTER", cx - Bwid, cy - Bhgt)
+			self.frame:SetPoint("TOPRIGHT",   hud, "CENTER", cx + Bwid, cy + Bhgt)
+			self.tex:SetTexCoord(TLx, TLy, BLx, BLy, TRx, TRy, BRx, BRy)
+		end
+		
+		return self:AddObject(l)
 	end
 end
 
@@ -255,7 +331,7 @@ do
 		end
 		
 		-- Track user raid target icon and class color
-		local display_rt = nil
+		local display_rt = -1
 		local display_class = nil
 		
 		function point:OnUpdate()
