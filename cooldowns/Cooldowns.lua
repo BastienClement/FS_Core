4f6adbf877581e1bc27514bb0983bd8a60efb832
Bastien Clément <g@ledric.me>
2016-03-28 20:10:47 +0200

    Cooldowns structure

---
+++ cooldowns/Cooldowns.lua
@@ -0,0 +1,15 @@
+local _, FS = ...
+local Cooldowns = FS:RegisterModule("Cooldowns")
+
+-------------------------------------------------------------------------------
+-- Life-cycle
+-------------------------------------------------------------------------------
+
+function Cooldowns:OnInitialize()
+end
+
+function Cooldowns:OnEnable()
+end
+
+function Cooldowns:OnDisable()
+end
