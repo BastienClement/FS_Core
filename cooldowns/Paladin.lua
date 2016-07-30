590a0671953fc01fe873eae5ee173c74b419e9a6
Bastien Clément <g@ledric.me>
2016-07-30 16:06:15 +0200

    Cooldowns: Update Paladin for Legion

--- cooldowns/Paladin.lua
+++ cooldowns/Paladin.lua
@@ -5,60 +5,68 @@ local SPEC_HOLY = 65
 local SPEC_PROTECTION = 66
 local SPEC_RETRIBUTION  = 70
 
---[[
+
 local function UnbreakableSpirit(base)
 	return function(unit)
 		local base_cooldown = type(base) == "function" and base(unit) or base
-		return unit:HasTalent(17591) and (base_cooldown / 2) or base_cooldown
+		return unit:HasTalent(17575) and (base_cooldown * 0.7) or base_cooldown
 	end
 end
-]]
 
 Cooldowns:RegisterSpells("PALADIN", {
 	[498] = { -- Divine Protection
-		cooldown = 60,
+		cooldown = UnbreakableSpirit(60),
 		duration = 8,
+		icon = 524353,
 		spec = { SPEC_HOLY, SPEC_RETRIBUTION }
-		-- TODO: Holy has Unbreakable Spirit
+		-- Holy Artifact inscreses the duration by 4 sec
 	},
 	[642] = { -- Divine Shield
-		cooldown = 300,
+		cooldown = UnbreakableSpirit(300),
 		duration = 8
-		-- TODO: Holy has Unbreakable Spirit
+		-- Prot Artifact makes it rechage faster with Forbearance
 	},
 	[633] = { -- Lay on Hands
-		cooldown = 600
-		-- TODO: Holy has Unbreakable Spirit
+		cooldown = UnbreakableSpirit(600)
+		-- TODO: Broken ?
+		-- Holy Artifact reduces cooldown by 30%
+		-- Prot Artifact makes it rechage faster with Forbearance
 	},
 	[1044] = { -- Blessing of Freedom
 		cooldown = 25,
 		duration = 6
+		-- Holy Artifact reduces cooldown by 15%
 	},
 	[1022] = { -- Blessing of Protection
 		cooldown = 300,
-		duration = 10
-		-- TODO: Replaced by Blessing of Spellwarding
+		duration = 10,
+		icon = 135964,
+		available = function(unit) return not unit:HasTalent(22433) end
+		-- Holy Artifact reduces cooldown by 15%
+		-- Prot Artifact makes it rechage faster with Forbearance
+		-- Retribution Artifact reduces cooldown by 30%
 	},
 	[6940] = { -- Blessing of Sacrifice
 		cooldown = 120,
 		duration = 12,
 		spec = { SPEC_HOLY, SPEC_PROTECTION }
+		-- Holy Artifact reduces cooldown by 15%
+		-- Prot Artifact reduces cooldown by 60 sec
 	},
 
 	-- Holy
 	[31842] = { -- Avenging Wrath (Holy)
 		cooldown = 120,
 		duration = function(unit)
-			return unit:HasTalent(17599) and 30 or 20
+			return unit:HasTalent(22190) and 30 or 20
 		end,
 		spec = SPEC_HOLY
-		-- TODO: Check talent
 	},
 	[31821] = { -- Aura Mastery
 		cooldown = 180,
-		duration = 6,
+		duration = 8,
 		spec = SPEC_HOLY
-		-- TODO: 10 sec with Artifact
+		-- Holy Artifact increases duration by 2 sec
 	},
 
 	-- Shared
@@ -78,6 +86,11 @@ Cooldowns:RegisterSpells("PALADIN", {
 		cooldown = 180,
 		duration = 10,
 		talent = true
-		-- Replaces Blessing of Protection
+		-- Prot Artifact makes it rechage faster with Forbearance
+	},
+	[105809] = { -- Holy Avenger
+		cooldown = 90,
+		duration = 20,
+		talent = true
 	},
 })
