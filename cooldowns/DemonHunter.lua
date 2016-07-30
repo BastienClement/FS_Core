a45a6774bd303c5470868c59896c9a8e6a068646
Bastien Clément <g@ledric.me>
2016-07-30 18:09:41 +0200

    Cooldowns: Update Demon Hunter for Legion

--- cooldowns/DemonHunter.lua
+++ cooldowns/DemonHunter.lua
@@ -4,45 +4,65 @@ local Cooldowns = FS:GetModule("Cooldowns")
 local SPEC_HAVOC = 577
 local SPEC_VENGEANCE = 581
 
+local function QuickenedSigils(base)
+	return function(unit)
+		return unit:HasTalent(22511) and (base * 0.8) or base
+	end
+end
+
 Cooldowns:RegisterSpells("DEMONHUNTER", {
 	[196718] = { -- Darkness
 		cooldown = 180,
 		duration = 8,
-	},
-	[179057] = { -- Chaos Nova
-		cooldown = 60,
-		duration = 5,
-		-- TODO: Consuming orbs reduces CD
 	},
 	[183752] = { -- Consume Magic
 		cooldown = 15,
 		duration = 3,
 	},
 
+	-- Havoc
+	[179057] = { -- Chaos Nova
+		cooldown = 60,
+		duration = 5,
+		spec = SPEC_HAVOC,
+		icon = 135795
+		-- TODO: Consuming orbs reduces CD
+	},
+	[212800] = { -- Blur
+		cooldown = 60,
+		duration = 10,
+		spec = SPEC_HAVOC,
+		icon = 1305150,
+		available = function(unit)
+			return (not unit:HasTalent(21863)) and (not unit:HasTalent(21864))
+		end,
+		enabled = false
+		-- BROKEN: no combat log event on cast
+	},
+
 	-- Vengeance
 	[202138] = { -- Sigil of Chains
-		cooldown = 120,
-		duration = 2,
+		cooldown = QuickenedSigils(120),
 		spec = SPEC_VENGEANCE
-		-- TODO: Duration & cooldown affected by talent (time to activate)
 	},
 	[202137] = { -- Sigil of Silence
-		cooldown = 60,
-		duration = 2,
+		cooldown = QuickenedSigils(60),
 		spec = SPEC_VENGEANCE
-		-- TODO: Duration & cooldown affected by talent (time to activate)
 	},
 	[207684] = { -- Sigil of Misery
-		cooldown = 60,
-		duration = 2,
+		cooldown = QuickenedSigils(60),
 		spec = SPEC_VENGEANCE
-		-- TODO: Duration & cooldown affected by talent (time to activate)
 	},
 
 	-- Talents
 	[207810] = { -- Nether Bond
 		cooldown = 120,
 		duration = 15,
+		talent = true
+	},
+	[196555] = { -- Netherwalk
+		cooldown = 90,
+		duration = 5,
 		talent = true
 	},
 })
